5998e137565ea0ebfb25746d1cb78360de02dbd0
boujnane monssef <m-heroine@hotmail.com>
2017-10-25 15:15:19 +0100

    trello auth

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-var express = require('express'),
-    app = express(),
-    setupHandlebars  = require('./app/setupHandlebars.js')(app),
-    setupPassport = require('./app/setupPassport'),
-    flash = require('connect-flash'),
-    appRouter = require('./app/routers/appRouter.js')(express),
-    session = require('express-session'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    jsonParser = bodyParser.json();
+var express = require("express"),
+  app = express(),
+  setupHandlebars = require("./app/setupHandlebars.js")(app),
+  setupPassport = require("./app/setupPassport"),
+  flash = require("connect-flash"),
+  appRouter = require("./app/routers/appRouter.js")(express),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
+  jsonParser = bodyParser.json();
 
-var port = process.env.PORT || 8081
+var port = process.env.PORT || 8888;
 
-app.use(cookieParser())
-app.use(session({ secret: '456',  saveUninitialized: true}))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser());
+app.use(session({ secret: "456", saveUninitialized: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/styles', express.static(__dirname + '/styles'))
+app.use("/styles", express.static(__dirname + "/styles"));
 //message
-app.use(flash())
+app.use(flash());
 app.use(function(req, res, next) {
-    res.locals.errorMessage = req.flash('error')
-    next()
+  res.locals.errorMessage = req.flash("error");
+  next();
 });
 
+setupPassport(app);
 
-
-setupPassport(app)
-
-app.use('/', appRouter)
+app.use("/", appRouter);
 
 // start app
-app.listen(port)
-console.log('Server started on port ' + port)
+app.listen(port);
+console.log("Server started on port " + port);
 
-module.exports.getApp = app
+module.exports.getApp = app;
